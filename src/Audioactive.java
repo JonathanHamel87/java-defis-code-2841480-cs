00fa3eddbd3cd24b790aca186f6f0091003bc31b
Jonathan Hamel <jonathan.hamel@desjardins.com>
2023-08-03 16:54:38 +0000

    solution exercice Désintégration audioactive

--- src/Audioactive.java
+++ src/Audioactive.java
@@ -1,6 +1,23 @@
 public class Audioactive {
 
   public static long valeurSuivante(long valeur) {
-    throw new UnsupportedOperationException("Essayez votre propre solution ici !!!");
+    long result = 0;
+    long previewValue = valeur % 10;
+    int counter = 1;
+    int power100 = 1;
+    do {
+      valeur /= 10;
+      long actualValue = valeur % 10;
+
+      if (actualValue != previewValue || valeur == 0) {
+        result += (10 * counter + previewValue) * power100;
+        previewValue = actualValue;
+        counter = 0;
+        power100 *= 100;
+      }
+      counter++;
+    } while (valeur > 0);
+
+    return result;
   }
 }
